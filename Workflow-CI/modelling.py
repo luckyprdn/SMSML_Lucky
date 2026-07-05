3b9aca6581cbaaef4e825db36011b5e15e8f9a3b
luckyprdn <pradanalucky168@gmail.com>
2026-07-05 19:59:26 +0700

    Ditch mlflow run, execute python directly and pass run_id.txt

--- Workflow-CI/modelling.py
+++ Workflow-CI/modelling.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
@@ -8,14 +7,13 @@ import dagshub
 import os
 import time
 
-# Otorisasi Headless menggunakan Token (Tidak butuh pop-up browser)
+# Otorisasi Headless menggunakan Token 
 os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/luckyprdn/Eksperimen_SML_Lucky.git"
 dagshub.init(repo_owner='luckyprdn', repo_name='Eksperimen_SML_Lucky', mlflow=True)
 mlflow.set_tracking_uri("https://dagshub.com/luckyprdn/Eksperimen_SML_Lucky.mlflow")
 
 def train_and_log_model():
     print("Membaca dataset...")
-    # Pastikan file CSV tersedia di dalam GitHub repository
     df = pd.read_csv('../Membangun_model/dataset/data_prepared.csv')
     X = df.drop(columns=['target'])
     y = df['target']
@@ -24,37 +22,27 @@ def train_and_log_model():
     
     mlflow.set_experiment("Automated_CI_Pipeline")
     
-    with mlflow.start_run():
+    with mlflow.start_run() as run:
         start_time = time.time()
-        
-        # Hyperparameters
-        params = {
-            "n_estimators": 100,
-            "max_depth": 10,
-            "random_state": 42
-        }
-        
+        params = {"n_estimators": 100, "max_depth": 10, "random_state": 42}
         mlflow.log_params(params)
         
         model = RandomForestClassifier(**params)
         model.fit(X_train, y_train)
         
-        end_time = time.time()
-        training_time = end_time - start_time
-        
+        training_time = time.time() - start_time
         predictions = model.predict(X_test)
         
-        # Metrik
-        acc = accuracy_score(y_test, predictions)
-        f1 = f1_score(y_test, predictions, average='weighted')
-        
-        mlflow.log_metric("accuracy", acc)
-        mlflow.log_metric("f1_score", f1)
+        mlflow.log_metric("accuracy", accuracy_score(y_test, predictions))
+        mlflow.log_metric("f1_score", f1_score(y_test, predictions, average='weighted'))
         mlflow.log_metric("training_time_seconds", training_time)
         
-        # Log model ke MLflow
         mlflow.sklearn.log_model(model, "model_pipeline")
-        print("Model berhasil di-training dan di-log ke DagsHub via CI/CD!")
+        print("Model berhasil di-training dan di-log!")
+
+        # SIMPAN RUN ID BUAT DIBACA SAMA GITHUB ACTIONS
+        with open("run_id.txt", "w") as f:
+            f.write(run.info.run_id)
 
 if __name__ == "__main__":
     train_and_log_model()
