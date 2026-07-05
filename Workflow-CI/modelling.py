599b4b030b3a0c3a820e639d323b8a27ee079f8a
Lucky Pradana <96008759+luckyprdn@users.noreply.github.com>
2026-07-05 21:23:28 +0700

    Add files via upload

--- Workflow-CI/modelling.py
+++ Workflow-CI/modelling.py
@@ -7,14 +7,13 @@ import dagshub
 import os
 import time
 
-# Otorisasi Headless menggunakan Token 
-os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/luckyprdn/Eksperimen_SML_Lucky.git"
+# Otorisasi Headless menggunakan Token
 dagshub.init(repo_owner='luckyprdn', repo_name='Eksperimen_SML_Lucky', mlflow=True)
 mlflow.set_tracking_uri("https://dagshub.com/luckyprdn/Eksperimen_SML_Lucky.mlflow")
 
 def train_and_log_model():
     print("Membaca dataset...")
-    
+
     # BLOK ANTI-GAGAL: Coba baca CSV, kalau nggak ada, langsung load dari library
     try:
         df = pd.read_csv('../Membangun_model/dataset/data_prepared.csv')
@@ -25,29 +24,29 @@ def train_and_log_model():
         data = load_breast_cancer()
         df = pd.DataFrame(data.data, columns=data.feature_names)
         df['target'] = data.target
-    
+
     X = df.drop(columns=['target'])
     y = df['target']
-    
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+
     mlflow.set_experiment("Automated_CI_Pipeline")
-    
+
     with mlflow.start_run() as run:
         start_time = time.time()
         params = {"n_estimators": 100, "max_depth": 10, "random_state": 42}
         mlflow.log_params(params)
-        
+
         model = RandomForestClassifier(**params)
         model.fit(X_train, y_train)
-        
+
         training_time = time.time() - start_time
         predictions = model.predict(X_test)
-        
+
         mlflow.log_metric("accuracy", accuracy_score(y_test, predictions))
         mlflow.log_metric("f1_score", f1_score(y_test, predictions, average='weighted'))
         mlflow.log_metric("training_time_seconds", training_time)
-        
+
         mlflow.sklearn.log_model(model, "model_pipeline")
         print("Model berhasil di-training dan di-log!")
 
